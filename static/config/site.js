407dfccfe5a571597b0827a1290173ef84f1b9d3
Ciao Chung <ciao0958@gmail.com>
2018-03-26 23:06:36 +0800

    加入Base Meta Tag

--- static/config/site.js
+++ static/config/site.js
@@ -1,7 +1,44 @@
 module.exports = {
   name: 'Ciao\'s Blog',
   meta: [
-
+    {
+      charset: 'utf-8'
+    },
+    {
+      vmid: 'keywords',
+      name: 'keywords',
+      content: 'Ciao\'s Blog,鍾板橋,板橋Blog',
+    },
+    {
+      vmid: 'description',
+      name: 'description',
+      content: '我是板橋',
+    },
+    {
+      vmid: 'og:title',
+      property: 'og:title',
+      content: 'Ciao\'s Blog',
+    },
+    {
+      vmid: 'og:description',
+      property: 'og:description',
+      content: '我是板橋',
+    },
+    {
+      vmid: 'og:site_name',
+      property: 'og:site_name',
+      content: 'Ciao\'s Blog',
+    },
+    {
+      vmid: 'og:url',
+      property: 'og:url',
+      content: window.location.href,
+    },
+    {
+      vmid: 'og:image',
+      property: 'og:image',
+      content: 'https://cdn.ciao.idv.tw/blog/main/main-sm.jpg',
+    },
   ],
   link: [
     { rel: 'favicon', href: '/static/favicon.ico' },
