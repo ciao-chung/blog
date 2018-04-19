d096b20d45ecd01ea851ab5689956d5a46560202
Ciao Chung <ciao0958@gmail.com>
2018-04-19 12:47:08 +0800

    更新seo.php, 處理SSR 404的狀況

--- copyfile/seo.php
+++ copyfile/seo.php
@@ -1,4 +1,20 @@
 <?php
+
+function parseHeaders( $headers ) {
+    $head = array();
+    foreach( $headers as $k=>$v ) {
+        $t = explode( ':', $v, 2 );
+        if( isset( $t[1] ) )
+            $head[ trim($t[0]) ] = trim( $t[1] );
+        else {
+            $head[] = $v;
+            if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                $head['reponse_code'] = intval($out[1]);
+        }
+    }
+    return $head;
+}
+
 $ssrHost = 'http://ssr.ciao-chung.com';
 
 $host = $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["SERVER_NAME"];
@@ -14,4 +30,9 @@ if($_SERVER["REQUEST_SCHEME"] == 'https' && $_SERVER["SERVER_PORT"] != 443){
 }
 $requestUrl = $host.$port.$_SERVER['REQUEST_URI'];
 $result = file_get_contents($ssrHost.'/'.$requestUrl);
+
+foreach($http_response_header as $headerLine) {
+    header($headerLine);
+}
+http_response_code(parseHeaders($http_response_header)['response-code']);
 echo $result;
